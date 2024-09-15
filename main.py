bb1c28232ebb2fa77206940e7b91032cb947dbbe
fndk <pyasecky.code@zohomail.eu>
2024-09-15 17:27:31 +0300

    Update

--- main.py
+++ main.py
@@ -76,6 +76,12 @@ async def webhook_update(update: WebhookUpdate, context: CustomContext) -> None:
     chat_member = await context.bot.get_chat_member(chat_id=update.user_id, user_id=update.user_id)
     payloads = context.user_data.setdefault("payloads", [])
     payloads.append(update.payload)
+    combined_payloads = "</code>\n• <code>".join(payloads)ы
+    text = (
+        f"The user {chat_member.user.mention_html()} has sent a new payload. "
+        f"So far they have sent the following payloads: \n\n• <code>{combined_payloads}</code>"
+    )
+    await context.bot.send_message(chat_id=ADMIN_CHAT_ID, text=text, parse_mode=ParseMode.HTML)
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
